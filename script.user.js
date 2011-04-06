3cec5837cc2c7a4ed2e3d29917d592416f3bec96
Luis Abreu <lmjabreu@gmail.com>
2011-04-06 20:32:27 +0100

    tweaked script trigger script, updated resources(apparently)

--- script.user.js
+++ script.user.js
@@ -5,36 +5,36 @@
 function replaceIcon() {
 	var title = document.getElementsByTagName('title')[0];
 	
-    if (title && title.innerText && title.innerText.search('Quodis') != -1) {
-    	var links = document.getElementsByTagName('link'),
-    	href,
-    	newHref,
-    	inboxElement = document.getElementById("canvas_frame").contentDocument.getElementsByClassName("n0").item(0),
-    	inboxTitle,
-    	unreadMsgCount = 0,
-    	matches;
-    	
-    	if (inboxElement) {
-    		inboxTitle = inboxElement.title;
-    		matches = inboxTitle.match(/\((\d*)\)/);
-    		
-    		if (matches) {
+	if (title && title.innerText && title.innerText.search('@quodis.com') != -1) {
+		var links = document.getElementsByTagName('link'),
+		href,
+		newHref,
+		inboxElement = document.getElementById("canvas_frame").contentDocument.getElementsByClassName("n0").item(0),
+		inboxTitle,
+		unreadMsgCount = 0,
+		matches;
+	
+		if (inboxElement) {
+			inboxTitle = inboxElement.title;
+			matches = inboxTitle.match(/\((\d*)\)/);
+
+			if (matches) {
 				unreadMsgCount = matches[1];
 			} else {
 				unreadMsgCount = 0;
 			}
-			
+
 			unreadMsgCount = (unreadMsgCount > 20) ? '20+' : unreadMsgCount;
     	}
-    	
-    	for (var i=links.length; i--; ) {
-    		if (links[i].rel == 'icon' || links[i].rel == 'shortcut icon') {
-    		    //href = links[i].href.split('/').reverse();
-    		    newHref = 'http://dl.dropbox.com/u/16123073/favicon/' + unreadMsgCount + '.png';
-    			links[i].href = newHref;
-    		}
-    	}
-    }
+
+		for (var i=links.length; i--; ) {
+			if (links[i].rel == 'icon' || links[i].rel == 'shortcut icon') {
+			    //href = links[i].href.split('/').reverse();
+			    newHref = 'http://dl.dropbox.com/u/16123073/favicon/' + unreadMsgCount + '.png';
+				links[i].href = newHref;
+			}
+		}
+	}
 }
 
 
